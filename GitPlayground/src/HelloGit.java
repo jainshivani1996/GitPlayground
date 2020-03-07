08d95ce3507653322aa31a78bc76bf11b72ee816
Training <Training@192.168.252.186>
2020-03-07 14:59:40 +0530

    a

--- GitPlayground/src/HelloGit.java
+++ GitPlayground/src/HelloGit.java
@@ -1,7 +1,7 @@
 
 public class HelloGit {
 public static void main(String[] args) {
-	System.out.println("Git  p roject ");
+	System.out.println("Git  pd roject ");
 	System.out.println( );   
 }
 }
